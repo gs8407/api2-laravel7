8e8f7d7b51e33ab37025d42e074b4ad7a5f439ad
shut_down <gs8407@gmail.com>
2020-04-04 14:46:22 +0200

    Reset Password

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -37,7 +37,7 @@ public $successStatus = 200;
             'name' => 'required',
             'email' => 'required|email',
             'password' => 'required',
-            'c_password' => 'required|same:password',
+            // 'c_password' => 'required|same:password',
         ]);
 if ($validator->fails()) {
             return response()->json(['error'=>$validator->errors()], 401);
--- routes/api.php
+++ routes/api.php
@@ -18,10 +18,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('login', 'API\UserController@login');
-Route::post('register', 'API\UserController@register');
+Route::post('/login', 'API\UserController@login');
+Route::post('/register', 'API\UserController@register');
+Route::post('/password/email', 'API\ForgotPasswordController@sendResetLinkEmail');
+Route::post('/password/reset', 'API\ResetPasswordController@reset');
 Route::group(['middleware' => 'auth:api'], function () {
-    Route::get('SafetyAndSecurity', 'API\SafetyAndSecurityController@show');
-    Route::get('Eula', 'API\EulaController@show');
-    Route::get('UserInfo', 'API\UserInfoController@show');
+    Route::get('/SafetyAndSecurity', 'API\SafetyAndSecurityController@show');
+    Route::get('/Eula', 'API\EulaController@show');
+    Route::get('/UserInfo', 'API\UserInfoController@show');
 });

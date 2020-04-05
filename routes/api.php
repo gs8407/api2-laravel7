c4d09a0b1ce28e875fe9d566065e636e7f35843b
shut_down <gs8407@gmail.com>
2020-04-05 12:57:28 +0200

    Refresh Token

--- routes/api.php
+++ routes/api.php
@@ -3,25 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
- */
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::post('/login', 'API\UserController@login');
+Route::post('/login', 'API\AccessTokenController@issueToken');
 Route::post('/register', 'API\UserController@register');
 Route::post('/password/email', 'API\ForgotPasswordController@sendResetLinkEmail');
 Route::post('/password/reset', 'API\ResetPasswordController@reset');
+
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('/SafetyAndSecurity', 'API\SafetyAndSecurityController@show');
     Route::get('/Eula', 'API\EulaController@show');

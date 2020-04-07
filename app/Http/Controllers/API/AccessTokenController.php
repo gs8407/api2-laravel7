d79db17d4ce960c65b0b8b28f45ced2c76e9979d
shut_down <gs8407@gmail.com>
2020-04-07 21:10:48 +0200

    Login error message setup

--- app/Http/Controllers/API/AccessTokenController.php
+++ app/Http/Controllers/API/AccessTokenController.php
@@ -31,8 +31,8 @@ class AccessTokenController extends ATC
             //convert json to array
             $data = json_decode($content, true);
 
-            if(isset($data["error"]))
-                throw new OAuthServerException('The user credentials were incorrect.', 6, 'invalid_credentials', 401);
+            if (isset($data["error"]))
+                throw new OAuthServerException('The user credentials were incorrect.', 'invalid_credentials', 401);
 
             //add access token to user
             $user = collect($user);
@@ -40,20 +40,21 @@ class AccessTokenController extends ATC
             $user->put('expires_in', $data['expires_in']);
             $user->put('refresh_token', $data['refresh_token']);
 
-            return Response::json(array($user));
+            // return Response::json(array($user));
 
-        }
-        catch (ModelNotFoundException $e) { // email notfound
+            return response(["access_token" => $data['access_token'], "expires_in" => $data['expires_in'], "refresh_token" => $data['refresh_token']]);
+
+        } catch (ModelNotFoundException $e) {
+            // email notfound
             //return error message
-            return response(["message" => "User not found"], 500);
-        }
-        catch (OAuthServerException $e) { //password not correct..token not granted
+            return response(["message" => "The user credentials are incorrect.", 'invalid_credentials', 401]);
+        } catch (OAuthServerException $e) {
+            //password not correct token not granted
             //return error message
-            return response(["message" => "The user credentials were incorrect.', 6, 'invalid_credentials"], 500);
-        }
-        catch (Exception $e) {
+            return response(["message" => "The user credentials are incorrect.", 'invalid_credentials', 401]);
+        } catch (Exception $e) {
             ////return error message
-            return response(["message" => "Internal server error"], 500);
+            return response(["message" => "The user credentials are incorrect.", 'invalid_credentials', 401]);
         }
     }
 }

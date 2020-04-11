1836d52043261cd5c2579b5750f60b28960b24b4
shut_down <gs8407@gmail.com>
2020-04-11 22:41:23 +0200

    Updated OAuth2

---
+++ app/Http/Controllers/API/SafetySecurityController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SafetySecurityController extends Controller
+{
+    public function show() {
+        return "Safety";
+    }
+}
